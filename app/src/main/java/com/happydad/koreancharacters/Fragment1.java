fe57c25733b63f98cc1c676f277e39a468bdf346
Joel <EH@English-House.local>
2014-07-06 00:26:02 +0900

    ㄱ fragment - added beta sounds

--- app/src/main/java/com/happydad/koreancharacters/Fragment1.java
+++ app/src/main/java/com/happydad/koreancharacters/Fragment1.java
@@ -1,5 +1,6 @@
 package com.happydad.koreancharacters;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -22,54 +23,181 @@ public class Fragment1 extends Fragment {
                 container, false);
 
 
-        //Add buttons for toast message and audio player
+        //Fragment section for button / audio
 
-        Button buttonga = (Button) mLinearLayout.findViewById(R.id.Buttonga);
-        buttonga.setOnClickListener(new View.OnClickListener() {
+        Button button_ga = (Button) mLinearLayout.findViewById(R.id.Button_ga);
+        button_ga.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Display a toast message in the fragment
-                Toast.makeText(getActivity(),"가",Toast.LENGTH_SHORT).show();
-                //TODO: trigger audio
+
+                MediaPlayer ga = MediaPlayer.create(getActivity(), R.raw.ga);
+                ga.start();
+
+                ga.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer ga) {
+                        ga.release();
+                    }
+                });
 
 
             }
         });
-        Button buttongya = (Button) mLinearLayout.findViewById(R.id.Buttongya);
-        buttongya.setOnClickListener(new View.OnClickListener() {
+
+        Button button_gya = (Button) mLinearLayout.findViewById(R.id.Button_gya);
+        button_gya.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Display a toast message in the fragment
-                Toast.makeText(getActivity(),"갸",Toast.LENGTH_SHORT).show();
-                //TODO: trigger audio
+
+                MediaPlayer gya = MediaPlayer.create(getActivity(), R.raw.gya);
+                gya.start();
+
+                gya.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer gya) {
+                        gya.release();
+                    }
+                });
 
             }
         });
 
-        Button buttongo = (Button) mLinearLayout.findViewById(R.id.Buttongo);
-        buttongo.setOnClickListener(new View.OnClickListener() {
+        Button button_geo = (Button) mLinearLayout.findViewById(R.id.Button_geo);
+        button_geo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Display a toast message in the fragment
-                Toast.makeText(getActivity(),"거",Toast.LENGTH_SHORT).show();
-                //TODO: trigger audio
+
+                MediaPlayer geo = MediaPlayer.create(getActivity(), R.raw.geo);
+                geo.start();
+
+                geo.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer geo) {
+                        geo.release();
+                    }
+                });
 
             }
         });
 
-        Button buttongyo = (Button) mLinearLayout.findViewById(R.id.Buttongyo);
-        buttongyo.setOnClickListener(new View.OnClickListener() {
+        Button button_gyeo = (Button) mLinearLayout.findViewById(R.id.Button_gyeo);
+        button_gyeo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Display a toast message in the fragment
-                Toast.makeText(getActivity(),"겨",Toast.LENGTH_SHORT).show();
-                //TODO: trigger audio
+
+                MediaPlayer gyeo = MediaPlayer.create(getActivity(), R.raw.gyeo);
+                gyeo.start();
+
+                gyeo.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer gyeo) {
+                        gyeo.release();
+                    }
+                });
 
             }
         });
 
+        Button button_goh = (Button) mLinearLayout.findViewById(R.id.Button_goh);
+        button_goh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaPlayer goh = MediaPlayer.create(getActivity(), R.raw.goh);
+                goh.start();
+
+                goh.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer goh) {
+                        goh.release();
+                    }
+                });
+
+            }
+        });
+
+        Button button_gyoh = (Button) mLinearLayout.findViewById(R.id.Button_gyoh);
+        button_gyoh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaPlayer gyoh = MediaPlayer.create(getActivity(), R.raw.gyoh);
+                gyoh.start();
+
+                gyoh.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer gyoh) {
+                        gyoh.release();
+                    }
+                });
+
+            }
+        });
+
+        Button button_goo = (Button) mLinearLayout.findViewById(R.id.Button_goo);
+        button_goo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaPlayer goo = MediaPlayer.create(getActivity(), R.raw.goo);
+                goo.start();
+
+                goo.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer goo) {
+                        goo.release();
+                    }
+                });
+
+            }
+        });
+
+        Button button_gyoo = (Button) mLinearLayout.findViewById(R.id.Button_gyoo);
+        button_gyoo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaPlayer gyoo = MediaPlayer.create(getActivity(), R.raw.gyoo);
+                gyoo.start();
+
+                gyoo.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer gyoo) {
+                        gyoo.release();
+                    }
+                });
+
+            }
+        });
+
+        Button button_geu = (Button) mLinearLayout.findViewById(R.id.Button_geu);
+        button_geu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaPlayer geu = MediaPlayer.create(getActivity(), R.raw.geu);
+                geu.start();
+
+                geu.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer geu) {
+                        geu.release();
+                    }
+                });
+
+            }
+        });
+
+        Button button_gee = (Button) mLinearLayout.findViewById(R.id.Button_gee);
+        button_gee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaPlayer gee = MediaPlayer.create(getActivity(), R.raw.gee);
+                gee.start();
+
+                gee.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer gee) {
+                        gee.release();
+                    }
+                });
+
+            }
+        });
+
+        //end return the layout
         return mLinearLayout;
-        //return inflater.inflate(R.layout.fragment_1,container,false);
 
     }
 }
